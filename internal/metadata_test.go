9c8ff427213bbcbfc2c84db4f8f379e82b096f62
Nic Johnson <nicjohnson145@hotmail.com>
2023-10-05 20:22:03 -0500

    refactor: add more test files

--- internal/metadata_test.go
+++ internal/metadata_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestComputeDestinationPath(t *testing.T) {
 	t.Run("smokes", func(t *testing.T) {
-		root := os.DirFS("./testdata/sample-files")
+		root := os.DirFS("./testdata/sample-files/Anders - 669")
 		got, err := computeDestinationPath(root, "02 - Diamonds.mp3")
 		require.NoError(t, err)
 		require.Equal(t, "Anders/669/02_Diamonds.mp3", got)
